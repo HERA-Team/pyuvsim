3c364b890518d0a6d636b8d7b66e1eb49c56b0d4
Matthew Kolopanis <Matthew.Kolopanis@gmail.com>
2021-03-09 15:36:39 -0700

    add mpi Tags

--- pyuvsim/mpi.py
+++ pyuvsim/mpi.py
@@ -26,6 +26,19 @@ status = None
 INT_MAX = 2**31 - 1
 
 
+def enum(*sequential, **named):
+    """Handy way to fake an enumerated type in Python
+    http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+    """
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
+
+
+# Define MPI message tags
+tags = enum('READY', 'DONE', 'EXIT', 'START')
+
+
+
 def set_mpi_excepthook(mpi_comm):
     """Kill the whole job on an uncaught python exception"""
 
